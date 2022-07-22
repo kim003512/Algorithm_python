a70d2bf723e4a3348ad959f9b43c2744c994f6ad
kim003512@naver.com <kim003512@naver.com>
2022-07-22 17:50:29 +0900

    단계별_문자열

---
+++ BaekJoon/단계별/문자열/10809.py
@@ -0,0 +1,7 @@
+inputStr = input()
+
+def listAlphabet() :
+    return list(string.ascii_lowercase)
+
+
+print(listAlphabet())
---
+++ BaekJoon/단계별/문자열/11720.py
@@ -0,0 +1,7 @@
+num = int(input())
+nums = input()
+sum = 0
+for i in range(num) :
+    sum += int(nums[i])
+
+print(sum)
