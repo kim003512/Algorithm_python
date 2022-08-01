f947c873a8330064d8fa3d304c9082ae711a27df
kim003512@naver.com <kim003512@naver.com>
2022-08-01 09:47:37 +0900

    블랙잭 - permutations, combinations 사용

--- BaekJoon/단계별/브루트 포스/2798.py
+++ BaekJoon/단계별/브루트 포스/2798.py
@@ -1,8 +1,11 @@
+from itertools import permutations, combinations
+
 N, M = map(int, input().split(' '))
 cards = list(map(int, input().split(' '))) #M만큼의 수가 들어오는지 확인?
 
 result = 0
 
+"""
 for i in range(0, len(cards)) :
     for j in range(i+1, len(cards)) :
         for k in range(j+1, len(cards)) :
@@ -11,4 +14,25 @@ for i in range(0, len(cards)) :
                 result = max(result, sum)
 
 print(result)
-                
+"""
+# permutations 사용
+nlist = []
+
+"""
+for three in permutations(cards, 3) :
+    if sum(three) > M :
+        continue
+    else :
+        nlist.append(sum(three))
+
+print(max(nlist))
+"""
+
+#combinations 사용
+for three in combinations(cards, 3) :
+    if sum(three) > M :
+        continue
+    else :
+        nlist.append(sum(three))
+
+print(max(nlist))
