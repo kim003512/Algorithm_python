8bdaf7d5469c92d98f1a544465b5a16388104e12
kim003512@naver.com <kim003512@naver.com>
2022-08-03 10:06:10 +0900

    단계별 - 정렬(수 정렬하기)

---
+++ BaekJoon/단계별/정렬/2750.py
@@ -0,0 +1,19 @@
+n = int(input())
+numList = [int(input()) for _ in range(n)]
+
+# numList.sort()
+
+#버블 정렬
+# for i in range(len(numList)) :
+#     for j in range(len(numList)) :
+#         if numList[i] < numList[j] :
+#              numList[i], numList[j] = numList[j], numList[i]
+
+#삽입정렬
+for i in range(1, len(numList)) :
+    while((i >= 0) and numList[i] < numList[i-1]) :
+        numList[i], numList[i-1] = numList[i-1], numList[i]
+        i -= 1
+
+for i in numList :
+    print(i)
